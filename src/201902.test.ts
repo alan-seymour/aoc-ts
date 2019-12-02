f382fee56186c4f8452f81a491fbaf0742027de6
Alan Seymour <alan@alanseymour.dev>
2019-12-02 14:33:30 +0800

    201902 completed

--- src/201902.test.ts
+++ src/201902.test.ts
@@ -1,32 +1,81 @@
-import { Puzzle201902, parseInput } from './201902';
+import { parseInput, SystemState, runToCompletetion, runOpcode } from './201902';
 
-let day: Puzzle201902;
-
-describe('201901', () => {
-  beforeEach(() => {
-    day = new Puzzle201902('');
-  });
+describe('201902', () => {
 
   test('parseInput', () => {
-    const input = `7
-    7
-    2
-    7
-    4`;
+    const input = `3500,9,10,70,2,3,11,0,99,30,40,50`;
     const result = parseInput(input);
-    expect(result).toEqual([7, 7, 2, 7, 4]);
+    expect(result).toEqual([3500, 9, 10, 70, 2, 3, 11, 0, 99, 30, 40, 50]);
   });
 
-  test('part1 example1', () => {
-    day.loadData(`12`);
-    const result = day.part1();
-    expect(result).toBe('2');
+  test('opcode 1', () => {
+    const input: SystemState = {
+      state: [1, 2, 3, 4],
+      index: 0,
+      halted: false,
+    };
+    const result = runOpcode(input);
+    expect(result).toEqual({state: [1, 2, 3, 4, 7], index: 4, halted: false});
   });
 
-  test('part2 example1', () => {
-    day.loadData(`1969`);
-    const result = day.part2();
-    expect(result).toBe('966');
+  test('opcode 2', () => {
+    const input: SystemState = {
+      state: [2, 2, 3, 4],
+      index: 0,
+      halted: false,
+    };
+    const result = runOpcode(input);
+    expect(result).toEqual({state: [2, 2, 3, 4, 12], index: 4, halted: false});
+  });
+
+  test('opcode 99', () => {
+    const input: SystemState = {
+      state: [99, 2, 3, 4],
+      index: 0,
+      halted: false,
+    };
+    const result = runOpcode(input);
+    expect(result).toEqual({state: [99, 2, 3, 4], index: 1, halted: true});
+  });
+
+  test('opcode ?', () => {
+    const input: SystemState = {
+      state: [4, 2, 3, 4],
+      index: 0,
+      halted: false,
+    };
+    const result = runOpcode(input);
+    expect(result).toEqual({state: [4, 2, 3, 4], index: 0, halted: false});
+  });
+
+  test('runToCompletion example 1', () => {
+    const input = [1, 9, 10, 3, 2, 3, 11, 0, 99, 30, 40, 50];
+    const result = runToCompletetion(9, 10, input);
+    expect(result).toEqual([3500, 9, 10, 70, 2, 3, 11, 0, 99, 30, 40, 50]);
+  });
+
+  test('runToCompletion example 2', () => {
+    const input = [1, 0, 0, 0, 99];
+    const result = runToCompletetion(0, 0, input);
+    expect(result).toEqual([2, 0, 0, 0, 99]);
+  });
+
+  test('runToCompletion example 3', () => {
+    const input = [2, 3, 0, 3, 99];
+    const result = runToCompletetion(3, 0, input);
+    expect(result).toEqual([2, 3, 0, 6, 99]);
+  });
+
+  test('runToCompletion example 4', () => {
+    const input = [2, 4, 4, 5, 99, 0];
+    const result = runToCompletetion(4, 4, input);
+    expect(result).toEqual([2, 4, 4, 5, 99, 9801]);
+  });
+
+  test('runToCompletion example 4', () => {
+    const input = [1, 1, 1, 4, 99, 5, 6, 0, 99];
+    const result = runToCompletetion(1, 1, input);
+    expect(result).toEqual([30, 1, 1, 4, 2, 5, 6, 0, 99]);
   });
 
 });
--- src/201902.ts
+++ src/201902.ts
@@ -2,17 +2,81 @@ import { splitLines } from './helpers';
 import { PuzzleDay } from './puzzleDay';
 
 export const parseInput = (input: string) => {
-  const lines = splitLines(input);
-  const numbers = lines.map(line => parseInt(line, 10));
+  const numbers = input.split(',').map(num => parseInt(num, 10));
   return numbers;
+};
+
+export type SystemState = {
+  state: number[]
+  index: number
+  halted: boolean
+};
+
+export const runOpcode = ({state, index, halted}: SystemState): SystemState => {
+  switch (state[index]) {
+    case 1:
+      state[state[index + 3]] = state[state[index + 2]] + state[state[index + 1]];
+      return {
+        state: [...state],
+        index: index + 4,
+        halted: false
+      };
+      break;
+    case 2:
+      state[state[index + 3]] = state[state[index + 2]] * state[state[index + 1]];
+      return {
+        state: [...state],
+        index: index + 4,
+        halted: false
+      };
+      break;
+    case 99:
+      return {
+        state: [...state],
+        index: index + 1,
+        halted: true
+      };
+      break;
+    default:
+      return {
+        state: [...state],
+        index,
+        halted
+      };
+  }
+};
+
+export const runToCompletetion = (noun: number, verb: number, initialState: number[]): number[] => {
+  let state: SystemState = {
+    state: [...initialState],
+    index: 0,
+    halted: false
+  };
+  state.state[1] = noun;
+  state.state[2] = verb;
+  while (!state.halted) {
+    state = runOpcode(state);
+  }
+  return state.state;
 };
 
 export class Puzzle201902 extends PuzzleDay {
   part1() {
-    return '';
+    const input = parseInput(this.input);
+    const state = runToCompletetion(12, 2, input);
+    return `${state[0]}`;
   }
 
   part2() {
-    return '';
+    const initialState = parseInput(this.input);
+    for (let noun = 0; noun < 100; noun++) {
+      for (let verb = 0; verb < 100; verb++) {
+        const result = runToCompletetion(noun, verb, initialState);
+        if (result[0] === 19690720) {
+          return `${(100 * noun) + verb}`;
+        }
+      }
+    }
+    return 'ERROR, No Solution Found';
   }
 }
