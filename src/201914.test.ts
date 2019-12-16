2e7ce9f3f3d4165c042714b59558744f61fd3012
Alan Seymour <alan@alanseymour.dev>
2019-12-16 17:23:12 +0800

    remove wrong tests

--- src/201914.test.ts
+++ src/201914.test.ts
@@ -1,52 +1,14 @@
-import { parseInput, runWithNounVerb } from './201902';
+import { parseInput } from './201914';
 
 describe('201902', () => {
   test('parseInput', () => {
-    const input = `3500,9,10,70,2,3,11,0,99,30,40,50`;
+    const input = `10 ORE => 10 A
+    1 ORE => 1 B
+    7 A, 1 B => 1 C
+    7 A, 1 C => 1 D
+    7 A, 1 D => 1 E
+    7 A, 1 E => 1 FUEL`;
     const result = parseInput(input);
-    expect(result).toEqual([3500, 9, 10, 70, 2, 3, 11, 0, 99, 30, 40, 50]);
-  });
-
-  test('runToCompletion example 1', () => {
-    const input = [1, 9, 10, 3, 2, 3, 11, 0, 99, 30, 40, 50];
-    const result = runWithNounVerb(input, 9, 10);
-    expect(result.state).toEqual([
-      3500,
-      9,
-      10,
-      70,
-      2,
-      3,
-      11,
-      0,
-      99,
-      30,
-      40,
-      50,
-    ]);
-  });
-
-  test('runToCompletion example 2', () => {
-    const input = [1, 0, 0, 2, 99];
-    const result = runWithNounVerb(input, 0, 0);
-    expect(result.state).toEqual([1, 0, 2, 2, 99]);
-  });
-
-  test('runToCompletion example 3', () => {
-    const input = [2, 3, 0, 3, 99];
-    const result = runWithNounVerb(input, 3, 0);
-    expect(result.state).toEqual([2, 3, 0, 6, 99]);
-  });
-
-  test('runToCompletion example 4', () => {
-    const input = [2, 4, 4, 5, 99, 0];
-    const result = runWithNounVerb(input, 4, 4);
-    expect(result.state).toEqual([2, 4, 4, 5, 99, 9801]);
-  });
-
-  test('runToCompletion example 4', () => {
-    const input = [1, 1, 1, 4, 99, 5, 6, 0, 99];
-    const result = runWithNounVerb(input, 1, 1);
-    expect(result.state).toEqual([30, 1, 1, 4, 2, 5, 6, 0, 99]);
+    expect(result.size).toEqual(6);
   });
 });
