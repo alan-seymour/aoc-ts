da9a140c0829fc50268773fc069ddd60dd288ea1
Alan Seymour <alan@alanseymour.dev>
2019-12-18 12:33:42 +0800

    added test

---
+++ src/201916.test.ts
@@ -0,0 +1,9 @@
+import { parseInput } from './201916';
+
+describe('201916', () => {
+  test('parseInput', () => {
+    const input = `1234556`;
+    const result = parseInput(input);
+    expect(result).toEqual([1, 2, 3, 4, 5, 5, 6]);
+  });
+});
