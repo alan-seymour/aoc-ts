06e385a46a3b7a233a0eb14dc801eaa0cb65896d
Alan Seymour <alan.seymour@healthengine.com.au>
2020-12-12 13:26:20 +0800

    Added file generation

---
+++ plopfile.ts
@@ -0,0 +1,40 @@
+import { NodePlopAPI } from 'plop';
+
+export default (plop: NodePlopAPI) => {
+  plop.setGenerator(
+    'day',
+    {
+      description: 'Create a new day file',
+      prompts: [{
+        type: 'input',
+        name: 'puzzleDate',
+        message: 'Puzzle date (eg. 202001)'
+      }],
+      actions: [
+        {
+          type: 'add',
+          path: 'src/solutions/{{puzzleDate}}.ts',
+          templateFile: '.plop-templates/solution.hbs'
+        },
+        {
+          type: 'add',
+          path: 'src/solutions/{{puzzleDate}}.test.ts',
+          templateFile: '.plop-templates/test.hbs'
+        },
+        {
+          type: 'modify',
+          path: 'src/solutions/index.ts',
+          pattern: /(.*Imports Here.*)/gi,
+          template: "import { Puzzle{{puzzleDate}} } from './{{puzzleDate}}'\;\n$1"
+        },
+        {
+          type: 'modify',
+          path: 'src/solutions/index.ts',
+          pattern: /(.*Maps Here.*)/gi,
+          template: "  {{puzzleDate}}: (input) => new Puzzle{{puzzleDate}}(input),\n$1"
+        }
+
+      ],
+    }
+  )
+}
